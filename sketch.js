89a87661a7687b2685378ed8c7648472f6a05619
cr34t1ve <mrsofua@gmail.com>
2021-12-17 16:26:23 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let color = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('black');
 }
 
 function draw() {
-  if(mouseIsPressed){}
+  if(mouseIsPressed){
+    fill(0, 0, color);
+  } else {
+    fill(225);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+  color = color + 5;
+  if (color > 254){
+    color = 0;
+  }
 }
